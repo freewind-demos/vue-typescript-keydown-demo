984f85d85263d524e1bc66667ae3a5016f0551ee
freewind <nowindlee@gmail.com>
2018-08-27 20:47:05 +0800

    demo

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-Vue Typescript Demo
-===================
+Vue Typescript KeyDown Demo
+===========================
 
 
 ```
 npm install
 npm run serve
 ```
+
+Resources
+---------
+
+- https://cn.vuejs.org/v2/guide/events.html#%E6%8C%89%E9%94%AE%E4%BF%AE%E9%A5%B0%E7%AC%A6
--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,49 @@
 <template>
-    <HelloWorld/>
+
+    <div>
+        <input type="text" autofocus placeholder="press up/down/left/right/enter"
+               v-on:keydown.up="pressUp()"
+               v-on:keydown.right="pressRight()"
+               v-on:keydown.down="pressDown()"
+               v-on:keydown.left="pressLeft()"
+               v-on:keydown.enter="pressEnter()"
+        >
+        <div>You pressed {{ key }}</div>
+    </div>
+
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from 'vue-property-decorator';
-    import HelloWorld from './components/HelloWorld.vue';
+    import {Component, Vue} from 'vue-property-decorator'
 
-    @Component({
-        components: {
-            HelloWorld,
-        },
-    })
+    @Component({})
     export default class App extends Vue {
+        key: string = ''
+
+        pressUp() {
+            this.key = 'up arrow'
+        }
+
+        pressDown() {
+            this.key = 'down arrow'
+        }
+
+        pressLeft() {
+            this.key = 'left arrow'
+        }
+
+        pressRight() {
+            this.key = 'right arrow'
+        }
+
+        pressEnter() {
+            this.key = 'enter'
+        }
     }
 </script>
+
+<style scoped>
+    input {
+        width: 400px
+    }
+</style>
